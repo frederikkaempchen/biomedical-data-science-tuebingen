80fdda94b6ae2275bd96e7509fd34bd899efbdfb
frederikkaempchen <frederikkaempchen@icloud.com>
2025-11-25 19:38:55 +0100

    - changed data structure of teamsData.json to better fit requirements - changed teamsScript and Styles accordingly moving toward new style of team page - not complete yet

--- wp-content/custom-code/pages/teams/teamsScript.js
+++ wp-content/custom-code/pages/teams/teamsScript.js
@@ -12,51 +12,51 @@ async function fetchTeams (pathname) {
 }
 
 async function displayTeams (pathname) {
-    const teams = await fetchTeams(pathname);
-    const teamsDiv = document.getElementById('teams')
-    teamsDiv.innerHTML = '';
+    const members = await fetchTeams(pathname);
+    const teamDiv = document.getElementById('team')
+    teamDiv.innerHTML = '';
 
-    for (const [teamId, teamObject] of Object.entries(teams)) {
-        const teamDiv = document.createElement('div');
-        teamDiv.id = `${teamId}`
-        teamDiv.classList.add('team')
-
-        const teamName = document.createElement('p')
-        teamName.classList.add('team-name')
-        teamName.textContent = teamObject.name
-        teamDiv.appendChild(teamName)
-
-        const teamDescription = document.createElement('p')
-        teamDescription.classList.add('team-description') 
-        teamDescription.textContent = teamObject.description 
-        teamDiv.appendChild(teamDescription)
-
-        const membersDiv = document.createElement('div')
-        membersDiv.classList.add('team-grid')
-        for (const [memberId, memberObject] of Object.entries(teamObject.members)) {
+    for (const [key, value] of Object.entries(members)) {
+        if (String(key).includes("SUBGROUP")) {
+            const subgroupDiv = document.createElement('div')
+            subgroupDiv.textContent = value
+            subgroupDiv.classList.add("subgroup")
+            teamDiv.appendChild(subgroupDiv)
+        }
+        else {
             const memberDiv = document.createElement('div')
             memberDiv.classList.add('team-member')
-            memberDiv.id = `${memberId}`
-
-            if ('break' in memberObject) {
-                memberDiv.style.gridColumnStart = memberObject.break;
-            }
+            memberDiv.id = `${key}`
 
             const memberImage = document.createElement('img')
             memberImage.classList.add('member-image')
-            memberImage.src = `${peopleImagesLocation}/${memberObject.image}.jpg`
-            memberImage.alt = memberObject.name
+            memberImage.src = `${peopleImagesLocation}/${value.image}.jpg`
+            memberImage.alt = value.name
             memberDiv.appendChild(memberImage)
+
+            const memberDataDiv = document.createElement('div')
+            memberDataDiv.classList.add('member-data')
 
             const memberName = document.createElement('p')
             memberName.classList.add('member-name')
-            memberName.textContent = memberObject.name
-            memberDiv.appendChild(memberName)
+            memberName.textContent = value.name
+            memberDataDiv.appendChild(memberName)
 
             const memberTitle = document.createElement('p')
             memberTitle.classList.add('member-title')
-            memberTitle.textContent = memberObject.title
-            memberDiv.appendChild(memberTitle)
+            memberTitle.textContent = value.title
+            memberDataDiv.appendChild(memberTitle)
+
+            if (value.text) {
+                const memberText = document.createElement('p')
+                memberText.classList.add('member-text')
+                memberText.textContent = value.text
+                memberDataDiv.appendChild(memberText)
+            }
+            
+
+            const iconsDiv = document.createElement('div')
+            iconsDiv.classList.add('icons')
 
             const mailDiv = document.createElement('div')
             mailDiv.classList.add("email-wrapper")
@@ -64,7 +64,7 @@ async function displayTeams (pathname) {
             const mailLink = document.createElement('a')
             mailLink.classList.add('email-link')
             mailLink.setAttribute('aria-label', 'email');
-            mailLink.setAttribute('data-encoded-mail', memberObject.mail);
+            mailLink.setAttribute('data-encoded-mail', value.mail);
 
             const mailIcon = document.createElement('span')
             mailIcon.classList.add('material-symbols-outlined')
@@ -73,13 +73,14 @@ async function displayTeams (pathname) {
 
             mailDiv.appendChild(mailLink)
 
-            memberDiv.appendChild(mailDiv)
-            
-            membersDiv.appendChild(memberDiv)
-        }
-        teamDiv.appendChild(membersDiv)
+            iconsDiv.appendChild(mailDiv)
 
-        teamsDiv.appendChild(teamDiv)
+            memberDataDiv.appendChild(iconsDiv)
+            
+            memberDiv.appendChild(memberDataDiv)
+
+            teamDiv.appendChild(memberDiv)
+        }
     }
 }
 
